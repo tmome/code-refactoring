f04710c1529bacf7a260ad77085af68facf89312
천현승 <woqkfehlfk@naver.com>
2023-06-03 17:57:45 +0900

    뒤엉킨 변경 before commit

---
+++ src/test/java/code/refactoring/_07_divergent_change/_25_move_function/AccountTest.java
@@ -0,0 +1,21 @@
+package code.refactoring._07_divergent_change._25_move_function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    void bankCharge() {
+        Account account = new Account(5, new AccountType(true));
+        assertEquals(14.5, account.getBankCharge());
+
+        account = new Account(8, new AccountType(true));
+        assertEquals(15.35, account.getBankCharge());
+
+        account = new Account(8, new AccountType(false));
+        assertEquals(18.5, account.getBankCharge());
+    }
+
+}
