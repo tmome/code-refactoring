52b787679373a4376e2f0c305c7de579e9e8c5fa
천현승 <woqkfehlfk@naver.com>
2023-06-03 17:36:00 +0900

    여러 함수를 변환 함수로 묶기 관련있는 여러 파생 변수를 만들어내는 함수가 여러곳에서 만들어지고 사용된다면 파생 변수를 변환 함수를 통해 한곳에 모아둘 수 있다. 기능의 확장의 목적으로 ReadingClient 을 상속 받아 여러곳에서 사용가능해짐.

--- src/main/java/code/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
+++ src/main/java/code/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client2.java
@@ -3,22 +3,14 @@ package code.refactoring._06_mutable_data._22_combine_functions_into_transform;
 import java.time.Month;
 import java.time.Year;
 
-public class Client2 {
+public class Client2 extends ReadingClient {
 
     private double base;
     private double taxableCharge;
 
     public Client2(Reading reading) {
-        this.base = baseRate(reading.month(), reading.year()) * reading.quantity();
+        this.base = enrichReading(reading).baseCharge();
         this.taxableCharge = Math.max(0, this.base - taxThreshold(reading.year()));
-    }
-
-    private double taxThreshold(Year year) {
-        return 5;
-    }
-
-    private double baseRate(Month month, Year year) {
-        return 10;
     }
 
     public double getBase() {
---
+++ src/main/java/code/refactoring/_06_mutable_data/_22_combine_functions_into_transform/EnrichReading.java
@@ -0,0 +1,5 @@
+package code.refactoring._06_mutable_data._22_combine_functions_into_transform;
+
+public record EnrichReading(Reading reading, double baseCharge) {
+
+}
