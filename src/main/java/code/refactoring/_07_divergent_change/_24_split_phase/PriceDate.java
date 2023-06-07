54b753e257a0860cb176838e24f3f3471c7fa225
천현승 <woqkfehlfk@naver.com>
2023-06-07 14:33:04 +0900

    단계 쪼개기 서로 각기 다른일을 하는 코드를 분리한다. -> 그래야 어떤 것을 변경할때 그것과 관련있는 것만 신경쓸 수 있다. 중간 데이터를 만들면 단계를 구별하고 매개변수를 줄이는데 도움이 된다.

---
+++ src/main/java/code/refactoring/_07_divergent_change/_24_split_phase/PriceDate.java
@@ -0,0 +1,5 @@
+package code.refactoring._07_divergent_change._24_split_phase;
+
+public record PriceDate(double basePrice, double discount, int quantity) {
+
+}
--- src/main/java/code/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/code/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,23 @@ package code.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceDate priceDate = calculatePriceData(product, quantity);
+        return applyShipping(priceDate, shippingMethod);
+    }
+
+    private PriceDate calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
+        final PriceDate priceDate = new PriceDate(basePrice, discount, quantity);
+        return priceDate;
+    }
+
+    private double applyShipping(PriceDate priceDate, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceDate.basePrice() > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
+        final double shippingCost = priceDate.quantity() * shippingPerCase;
+        final double price = priceDate.basePrice() - priceDate.discount() + shippingCost;
         return price;
     }
 }
