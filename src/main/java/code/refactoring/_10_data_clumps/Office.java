0fe872166b510518aebdbbd285c1e3d2ea78b467
천현승 <woqkfehlfk@naver.com>
2023-07-17 16:01:36 +0900

    데이터 뭉치 항상 뭉쳐다니는 데이터는 한 곳에 모아두는것이 좋다.

--- src/main/java/code/refactoring/_10_data_clumps/Office.java
+++ src/main/java/code/refactoring/_10_data_clumps/Office.java
@@ -4,33 +4,20 @@ public class Office {
 
     private String location;
 
-    private String officeAreCode;
+    private TelephoneNumber officePhoneNumber;
 
-    private String officeNumber;
-
-    public Office(String location, String officeAreCode, String officeNumber) {
+    public Office(String location, TelephoneNumber officePhoneNumber) {
         this.location = location;
-        this.officeAreCode = officeAreCode;
-        this.officeNumber = officeNumber;
+        this.officePhoneNumber = officePhoneNumber;
     }
 
     public String officePhoneNumber() {
-        return officeAreCode + "-" + officeNumber;
+        return this.officePhoneNumber().toString();
     }
 
-    public String getOfficeAreCode() {
-        return officeAreCode;
-    }
 
-    public void setOfficeAreCode(String officeAreCode) {
-        this.officeAreCode = officeAreCode;
-    }
 
-    public String getOfficeNumber() {
-        return officeNumber;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
+    public TelephoneNumber getOfficePhoneNumber() {
+        return officePhoneNumber;
     }
 }
