7bf760ebdd0eb7785f0b21bffc67e0802af29ae2
천현승 <woqkfehlfk@naver.com>
2023-07-25 21:55:19 +0900

    before commit

---
+++ src/test/java/code/refactoring/_12_repeated_swtiches/SwitchImprovementsTest.java
@@ -0,0 +1,15 @@
+package code.refactoring._12_repeated_swtiches;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SwitchImprovementsTest {
+
+    @Test
+    void vacationHours() {
+        SwitchImprovements si = new SwitchImprovements();
+        assertEquals(120, si.vacationHours("full-time"));
+    }
+
+}
